bcdca07ebfdd33b15068364687fa06b8674a6570
Mike Branchini <mikebranchini@gmail.com>
2013-06-26 15:09:34 -0700

    initial commit on aure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = '9b1670ba1c0c3ac1ec466d0395ff4b3ab1e71af891a066a476733a8da23ac80d21525eff97acd0941c7fd1f9654eccb31f1d7267bd78d3e65def164e52d4d334'
